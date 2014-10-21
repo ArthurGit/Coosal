2f7098c9e889c9e3e383f2f017ff1a2eac839178
Arthur.Zhang <Arthur.Zhang@innocellence.com>
2014-10-21 20:52:25 +0800

    Coosal 初始化

---
+++ Sale/Coosal-Sale/Coosal-Sale/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Coosal-Sale
+//
+//  Created by Zhang Liang on 10/21/14.
+//  Copyright (c) 2014 Coosal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
