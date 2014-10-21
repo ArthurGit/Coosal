e4c61b676f9ee4d71dd1f3b42e45efeef4d99f90
344309634@qq.com <344309634@qq.com>
2014-10-21 21:59:34 +0800

    update code

---
+++ Menu/Coosal-Menu/Coosal-Menu/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Coosal-Menu
+//
+//  Created by 张良 on 14/10/21.
+//  Copyright (c) 2014年 Coosal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
